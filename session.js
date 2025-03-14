2c30db072edc7ddaa8db0e8fd404a214cd486a52
KIMG-LK <dgimshan234@gmail.com>
2025-03-14 19:00:41 +0530

    Update session.js

--- session.js
+++ session.js
@@ -43,5 +43,5 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "QUEEN-XPRO~H2pCCaxB#fVp1QjXDNxrdJN16o9csEkiO5oAySwEF_S1xTzmuMBk",
 SUDO: process.env.SUDO || ["94717775628","94758775628"],
 PORT: process.env.PORT || "8000",
-FOOTER: process.env.FOOTER || "> QUEEN ANJU XPRO\n> Developed by Mr. Rashmika\n> GitHub Repository: github.com/Mrrashmika/QUEEN_ANJU_xPro\n🌟 Premium Version | Advanced Features | Secure & Reliable 🌟\n© 2025 QUEEN ANJU XPRO. All rights reserved.",
+FOOTER: process.env.FOOTER || "> POWERED BY KING CINEMA\n> SRILANKA NO1 BOT FAMILY..",
 };
